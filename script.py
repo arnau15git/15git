4176eb344c2592ed0fa890742fdb96509b9d93a2
arnau15git <35104462+arnau15git@users.noreply.github.com>
2018-01-10 20:34:38 +0000

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,40 @@
 
+#!usr/bin/env python
+#-*- coding: utf-8 -*-
+
+
+abc = "abcdefghijklmopqvrstxyz"
+
+  
+def crypt(cadena, clave):
+    text_cifrado = ''
+
+    for letra in cadena:
+        suma = abc.find(letra) + clave
+        modulo = int(suma) % len(abc)
+        text_cifrado = text_cifrado + str(abc[modulo])
+
+    return text_cifrado
+
+def decrypt(cadena, clave):
+    text_cifrado = ''
+
+    for letra in cadena:
+        resta = abc.find(letra) - clave
+        modulo = int(resta) %len(abc)
+        text_cifrado = text_cifrado + str(abc[modulo])
+
+
+def main():
+    c = str(raw_input("Pon aquí la cadena de texto para encriptar:")).lower()
+    n = int(raw_input("Pon aquí la clave:"))
+    print crypt(c,n)
+    nc = str(raw_input("Pon aquí la clave para desencriptar: ")).lower()
+    nn = int(raw_input("Pon aquí la clave: "))
+    print decrypt(nc, nn)
+
+
+
+
+if __name__ == '__main__':
+    main()
